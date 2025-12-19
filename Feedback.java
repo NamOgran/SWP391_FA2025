3f838b385ec38d400a39e702f81bb6cbc7355dc8
PhuocNNT <phuocnntce182513@fpt.edu.vn>
2025-12-19 09:16:21 +0700

    dữ liệu feedback

---
+++ Feedback.java
@@ -0,0 +1,100 @@
+package entity;
+
+import java.sql.Timestamp;
+
+public class Feedback {
+
+    private int feedbackId;
+    private int customerId;
+    private int productId;
+    private int orderId;
+    private int ratePoint;
+    private String content;
+    private Timestamp createdAt;
+    private String customerName;
+    private String productName;
+    private String productImage;
+
+    public Feedback() {
+    }
+
+    public int getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(int feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getRatePoint() {
+        return ratePoint;
+    }
+
+    public void setRatePoint(int ratePoint) {
+        this.ratePoint = ratePoint;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+}
